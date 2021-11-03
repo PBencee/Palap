6f4f401951438ffe6757c704464d602275d4430a
PBence <90601408+PBencee@users.noreply.github.com>
2021-11-03 14:47:51 +0100

    Add files via upload

---
+++ dobogenyo.py
@@ -0,0 +1,26 @@
+import random
+a = random.randrange(1,7)
+if a == 1:
+    print('   o  o  o ')
+    print('   o  @  o ')
+    print('   o  o  o ')
+elif a == 2:
+    print('   o  o  o ')
+    print('   @  o  @ ')
+    print('   o  o  o ')
+elif a == 3:
+    print('   o  o  @ ')
+    print('   o  @  o ')
+    print('   @  o  o ')
+elif a == 4:
+    print('   @  o  @ ')
+    print('   o  o  o ')
+    print('   @  o  @ ')
+elif a == 5:
+    print('   @  o  @ ')
+    print('   o  @  o ')
+    print('   @  o  @ ')
+elif a == 6:
+    print('   @  o  @ ')
+    print('   @  o  @ ')
+    print('   @  o  @ ')
